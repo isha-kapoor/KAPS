557eb87909b2dd077a675d144812e41d1ed52dd6
isha-kapoor <ishakapoorsofia@gmail.com>
2021-03-27 16:28:32 +0530

    Bug Fix and Farmer's Pages

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,21 +1,66 @@
 const jwt = require("jsonwebtoken");
 const CRegister = require("../models/ccregisters")
 
-const auth = async(req,res,next) =>{
-    try {
-        const new_token = req.cookies.jwt;
-        const verifyUser =await jwt.verify(new_token , process.env.SECRET_KEY)
-        console.log(verifyUser);
+// const auth = async(req,res,next) =>{
+//     try {
+//         const new_token = req.cookies.jwt;
+//         const verifyUser =await jwt.verify(new_token , process.env.SECRET_KEY)
+//         console.log(verifyUser);
+//
+//         const userData = await CRegister.findOne({_id:verifyUser._id});
+//         console.log(userData);
+//
+//         req.token = new_token;
+//         req.user = userData;
+//
+//         next();
+//     } catch (e) {
+//       res.status(401).send(e);
+//     }
+// }
 
-        const userData = await CRegister.findOne({_id:verifyUser._id});
-        console.log(userData);
+// const authrole = async(req,res,next)=>{
+//   try{
+//     if(select===req.user.select){
+//     next();
+//   }
+//   else res.status(401).send(e)
+//   }catch(e){
+//     res.status(401).send(e);
+//   }
+// }
 
-        req.token = new_token;
-        req.user = userData;
+function auth() {
+  return async(req,res,next)=>{
+      try {
+          const new_token = req.cookies.jwt;
+          const verifyUser =await jwt.verify(new_token , process.env.SECRET_KEY)
+          console.log(verifyUser);
 
-        next();
-    } catch (e) {
-      res.status(401).send(e);
-    }
+          const userData = await CRegister.findOne({_id:verifyUser._id});
+          console.log(userData);
+
+          req.token = new_token;
+          req.user = userData;
+
+          next();
+      } catch (e) {
+        res.status(401)
+        return res.send("E")
+      }
 }
-module.exports = auth;
+}
+
+function authrole(select) {
+  return (req,res,next)=>{
+    if(req.user.select !== select){
+      res.status(401)
+      return res.send("You are not authorized to open this page");
+    }
+    next();
+  }
+}
+module.exports = {
+  auth,
+  authrole
+}
